c2f023fe447a8320726623dad696d864cf21e715
Natan Pires <natan.void@hotmail.com>
2025-03-15 23:54:55 -0300

    Adicionando novos arquivos antes do merge

---
+++ src/main/kotlin/com/picpayChallenge/infra/GlobalExceptionHandler.kt
@@ -0,0 +1,9 @@
+package com.picpayChallenge.infra
+
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.nio.file.attribute.UserPrincipalNotFoundException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+}
